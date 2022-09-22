755e72f3121de22c5ade86d884579abf14eb4b11
Casres <christian@casillas.tech>
2022-09-22 01:28:18 -0400

    Added mongoose to server.js and connected the server to mongoose

---
+++ models/index.js
@@ -0,0 +1,10 @@
+// gets all of the models and puts them here, 
+// acts as a directory if you would, for all of the 
+// models for this website. The Pizz-Hunt application.
+
+
+
+// requires the Pizza model schema from the pizza.js file in the models folder
+const Pizza = require('./Pizza');
+
+module.exports = [ Pizza ]; 
