7e2082cc332c08b80d0df01b44186248056a1890
Casres <christian@casillas.tech>
2022-10-02 17:25:20 -0400

    Created pizza routes code/syntax

--- routes/api/pizza-routes.js
+++ routes/api/pizza-routes.js
@@ -1,16 +1,23 @@
 const router = require('express').Router();
+const {
+    getAllPizza,
+    getPizzaById,
+    createPizza,
+    updatePizza,
+    deletePizza
+} = require('../../controllers/pizza-controller');
 
 // gets ALL and POST at 'api/pizzas' (api route)
 router
 .route('/')
-.get()
-.post();
+.get(getAllPizza)
+.post(createPizza);
 
 // gets ONE, PUT and DELETE at '/api/pizzas/:id' (api route)
 router
 .route('/:id')
-.get()
-.put()
-.delete();
+.get(getPizzaById)
+.put(updatePizza)
+.delete(deletePizza);
 
 module.exports = router; 
